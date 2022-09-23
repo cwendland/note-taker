628b5b026440c4d62ea8cf8ea8c4076f0291e64d
Chris Wendland <chriswendland@Chriss-MBP.lan>
2022-09-23 15:52:39 -0400

    Added helper UUID function and added a delete route

---
+++ helper/uuid.js
@@ -0,0 +1,5 @@
+const uuid = () => {
+    return Math.floor(Math.random()*1000);
+}
+
+module.exports = uuid;
--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const fs = require('fs');
-const notes = require('../../db/db.json')
+const notes = require('../../db/db.json');
+const uuid = require('../../helper/uuid');
 
 router.get('/notes', (req,res) => {
     res.json(notes);
@@ -8,10 +9,20 @@ router.get('/notes', (req,res) => {
 
 router.post('/notes', (req,res) => {
     //Append an ID property to the note with a unique ID for each note
+    const newID = uuid();
     const newNote = req.body;
-    newNote.id = /*TODO add ID*/ 3;
+    newNote.id = newID;
     fs.appendFile('../../db/db.json', JSON.stringify(req.body), (err) => console.log(err));
     res.send(newNote);
+});
+
+router.delete('/notes/:id', (req,res) => {
+    //Find the note by ID and then remove it from the json
+    notes.forEach(element => {
+        if(element.id === req.params.id) {
+            
+        }
+    });
 });
 
 module.exports = router;
