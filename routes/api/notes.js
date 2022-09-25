760c62c520f915135ff74a1d4b099b36864f7457
cwendland <chris.r.wendland@gmail.com>
2022-09-25 14:16:28 -0700

    Added delete functionality for express and add functionality to the database json

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -9,20 +9,20 @@ router.get('/notes', (req,res) => {
 
 router.post('/notes', (req,res) => {
     //Append an ID property to the note with a unique ID for each note
+    let newNotes = JSON.parse(notes);
     const newID = uuid();
     const newNote = req.body;
     newNote.id = newID;
-    fs.appendFile('../../db/db.json', JSON.stringify(req.body), (err) => console.log(err));
+    newNotes.push(newNote);
+    fs.writeFile('../../db/db.json', JSON.stringify(newNotes), (err) => console.log(err));
     res.send(newNote);
 });
 
 router.delete('/notes/:id', (req,res) => {
     //Find the note by ID and then remove it from the json
-    notes.forEach(element => {
-        if(element.id === req.params.id) {
-            
-        }
-    });
+    let newNotes = notes.filter(note => note.id !== req.params.id);
+    fs.writeFile('../../db/db.json', JSON.stringify(newNotes), (err) => console.log(err));
+    res.send(`Deleted note with id ${req.params.id}`);
 });
 
 module.exports = router;
